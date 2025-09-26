2608b77165e7fc3f2a96454a8b4dc98c9ad0d8c6
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2025-09-26 18:58:55 +0900

    필요없는 페이지 제거

--- frontend/src/components/resumeoverview_old/ResumePageGroup.tsx
+++ frontend/src/components/resumeoverview_old/ResumePageGroup.tsx
@@ -1,11 +1,11 @@
+import { FeedbackPoint } from "@/types/FeedbackPointType";
 import ResumePage from "./ResumePage";
-import { AddFeedbackPoint, FeedbackPoint } from "../../types.ts";
 
 type ResumePageGroupProps = {
   feedbackPoints: FeedbackPoint[];
-  addFeedbackPoint: (point: Omit<AddFeedbackPoint, "id" | "type">) => void;
-  deleteFeedbackPoint: (id: number) => void;
-  editFeedbackPoint: (item: AddFeedbackPoint) => void;
+  // addFeedbackPoint: (point: Omit<AddFeedbackPoint, "id" | "type">) => void;
+  // deleteFeedbackPoint: (id: number) => void;
+  // editFeedbackPoint: (item: AddFeedbackPoint) => void;
   hoveredCommentId: number | null;
   setHoveredCommentId: (id: number | null) => void;
   setClickedCommentId: (id: number | null) => void;
@@ -13,9 +13,9 @@ type ResumePageGroupProps = {
 
 function ResumePageGroup({
   feedbackPoints,
-  addFeedbackPoint,
-  deleteFeedbackPoint,
-  editFeedbackPoint,
+  // addFeedbackPoint,
+  // deleteFeedbackPoint,
+  // editFeedbackPoint,
   hoveredCommentId,
   setHoveredCommentId,
   setClickedCommentId,
@@ -26,9 +26,9 @@ function ResumePageGroup({
       key={1}
       pageNumber={1}
       feedbackPoints={feedbackPoints}
-      addFeedbackPoint={addFeedbackPoint}
-      deleteFeedbackPoint={deleteFeedbackPoint}
-      editFeedbackPoint={editFeedbackPoint}
+      // addFeedbackPoint={addFeedbackPoint}
+      // deleteFeedbackPoint={deleteFeedbackPoint}
+      // editFeedbackPoint={editFeedbackPoint}
       hoveredCommentId={hoveredCommentId}
       setHoveredCommentId={setHoveredCommentId}
       setClickedCommentId={setClickedCommentId}
